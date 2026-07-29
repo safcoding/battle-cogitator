e13c0cdc2a7cbe584f2c94a46c1996e1524e015c
Safuan Hakim <155789635+safcoding@users.noreply.github.com>
2026-07-29 22:02:32 +0800

    seperated datasheet

---
+++ datasheet.py
@@ -0,0 +1,120 @@
+astartes_unit = {
+    "name": "Assault Intercessor Squad",
+    "M" : 6,
+    "T" : 4,
+    "SV": 3,
+    "LD": 6,
+    "OC": 2,
+    "keywords": ["INFANTRY", "BATTLELINE", "GRENADES", "IMPERIUM", "TACTICUS", "ASSAULT INTERCESSOR SQUAD"],
+
+    "models": [
+        {
+            "name": "Intercessor Sergeant",
+            "max_W" : 2,
+            "curr_W": 2,   
+            "r_weapon": "plasma_pistol",
+            "m_weapon": "power_fist",
+        },
+        {
+            "name": "Assault Intercessor",
+            "max_W" : 2,
+            "curr_W": 2,   
+            "r_weapon": "heavy_bolt_pistol",
+            "m_weapon": "chainsword"
+        },
+        {
+            "name": "Assault Intercessor",
+            "max_W" : 2,
+            "curr_W": 2,   
+            "r_weapon": "heavy_bolt_pistol",
+            "m_weapon": "chainsword"
+        },
+        {
+            "name": "Assault Intercessor",
+            "max_W" : 2,
+            "curr_W": 2,   
+            "r_weapon": "heavy_bolt_pistol",
+            "m_weapon": "chainsword"
+        },
+        {
+            "name": "Assault Intercessor",
+            "max_W" : 2,
+            "curr_W": 2,   
+            "r_weapon": "heavy_bolt_pistol",
+            "m_weapon": "chainsword"
+        }
+    ]
+}
+
+khorne_unit = {
+    "name": "Bloodletters",
+    "M" : 6,
+    "T" : 4,
+    "SV": 7,
+    "LD": 7,
+    "OC": 2,
+    "keywords": ["INFANTRY", "BATTLELINE", "CHAOS", "DAEMON", "KHORNE", "BLOODLETTERS" ],
+
+    "models": [
+        {
+            "name": "Bloodreaper",
+            "max_W" : 1,
+            "curr_W": 1,   
+            "m_weapon": "hellblade",
+        },
+        {
+            "name": "Bloodletter",
+            "max_W" : 1,
+            "curr_W": 1,   
+            "m_weapon": "hellblade",
+        },
+        {
+            "name": "Bloodletter",
+            "max_W" : 1,
+            "curr_W": 1,   
+            "m_weapon": "hellblade",
+        },
+        {
+            "name": "Bloodletter",
+            "max_W" : 1,
+            "curr_W": 1,   
+            "m_weapon": "hellblade",
+        },
+        {
+            "name": "Bloodletter",
+            "max_W" : 1,
+            "curr_W": 1,   
+            "m_weapon": "hellblade",
+        },
+        {
+            "name": "Bloodletter",
+            "max_W" : 1,
+            "curr_W": 1,   
+            "m_weapon": "hellblade",
+        },
+        {
+            "name": "Bloodletter",
+            "max_W" : 1,
+            "curr_W": 1,   
+            "m_weapon": "hellblade",
+        },
+        {
+            "name": "Bloodletter",
+            "max_W" : 1,
+            "curr_W": 1,   
+            "m_weapon": "hellblade",
+        },
+        {
+            "name": "Bloodletter",
+            "max_W" : 1,
+            "curr_W": 1,   
+            "m_weapon": "hellblade",
+        },
+        {
+            "name": "Bloodletter",
+            "max_W" : 1,
+            "curr_W": 1,   
+            "m_weapon": "hellblade",
+        },
+    ]
+}
